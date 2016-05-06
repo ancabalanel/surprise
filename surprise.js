c239c308d5dc217168abedc06c96a570020b2161
Anca Balanel <ancabalanel@192-168-0-101.rdsnet.ro>
2016-05-06 11:21:07 +0300

    sending requests

--- surprise.js
+++ surprise.js
@@ -1,11 +1,13 @@
-var root = 'http://jsonplaceholder.typicode.com';
+$(document).ready( function() {
+	var root = 'http://jsonplaceholder.typicode.com';
 
 
-$("#button1").click(function(){
-	$.ajax({
-	  url: root + '/posts/1',
-	  method: 'GET'
-	}).then(function(data) {
-	  console.log(data);
-	});
-})
+	$("#button1").click(function(){
+		$.ajax({
+		  url: root + '/posts/1',
+		  method: 'GET'
+		}).then(function(data) {
+		  console.log(data);
+		});
+	})
+});
